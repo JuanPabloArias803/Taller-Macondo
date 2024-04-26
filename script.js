fb7bdc143f4dea816b63c0875d5a93f471a0c797
JuanPabloArias803 <juanpabloarias803@gmail.com>
2024-04-26 17:03:12 -0500

    upload changes, final version

--- script.js
+++ script.js
@@ -189,11 +189,165 @@ const macondoDays=function(){
                 }
             }
         }else if(colorSelect=="Rojo"){
-            alert(colorSelect);
+            alert("La actividad para el día de hoy es disfrutar de la maravillosa playa privada del hotel. Preparate para la arena, el sol y el mar!");
+            alert("Al llegar a la playa, un empleado del hotel te recuerda que tienes acceso bar todo incluído.")
+            let j=true;
+            while(j){
+                choice=Number(prompt("¿Te gusta el alcohol?\n1. Si\n2. No"));
+                if((choice>0)&&(choice<3)){
+                    if(choice==1){
+                        alert("En el día juegas voleibol, nadas en el mar, y montas en moto acuatica. Todo es genial pero...");
+                        alert("El exeso de alcohol te produce dolores de cabeza y comienzas a perder la visión. Tienes que ir al hospital por lo que terminan tus vacaciones.");
+                        j=false;
+                        return;
+                    }else if(choice==2){
+                        alert("En el día juegas voleibol, nadas en el mar y montas en moto acuatica. Todo es genial.");
+                        alert("Terminas tu día de maravilla, eres consciente de que el alcohol no es necesario para divertirse");
+                        j=false;
+                    }
+                }else{
+                    alert("Ingresa una opción válida");
+                }
+            }
         }else if(colorSelect=="Azul"){
-            alert(colorSelect);
+            alert("La actividad para el día de hoy es disfrutar de los salones que el hotel dispone para sus huespedes. El hotel cuenta con salón de baile, salón de bingo y casino.");
+            alert("Debes decidir a cuál de los tres salones asistir durante el día.");
+            let j=true;
+            while(j){
+                choice=Number(prompt("¿Que salón prefieres?\n1. Salón de baile.\n2. Salón de Bingo.\n3. Casino."));
+                if((choice>0)&&(choice<4)){
+                    if(choice==1){
+                        choice=confirm("Bailas todo el día con una persona hermosa. Te invita a pasar la noche juntos ¿Aceptas?");
+                        if(choice){
+                            alert("Pasan una noche maravillosa juntos, pero al despertar te das cuenta que se fue y te robo tu dinero.");
+                            budget=0;
+                            showBudget(budget);
+                        }else{
+                            alert("Decides pasar de la propuesta ya que no te interesan las relaciones pasajeras. Tu día termina bien bailando con otras personas.");
+                        }
+                        j=false;
+                    }else if(choice==2){
+                        alert("Cuando llegas al salón de bingo, un recepcionista te avisa que la entrada al juego vale 100000.");
+                        let bingoPrice=100000;
+                        if (bingoPrice<budget){
+                            choice=confirm("¿Deseas entrar al bingo?");
+                            if(choice){
+                                budget-=bingoPrice;
+                                showBudget(budget);
+                                let bingoWin=Math.floor(Math.random()*11);
+                                if(bingoWin==0){
+                                    alert("Ganaste el premio mayor del bingo!!!\nFelicidades, acabas de ganar el premio mayor del bingo, el premio son 5000000");
+                                    budget+=5000000;
+                                    showBudget(budget);
+                                }else{
+                                    alert("No ganas en el bingo, pero disfrutas mucho el juego y el buffet ofrecido, tu día termina bien.")
+                                }
+                            }else{
+                                alert("Decides no apostar al bingo, pero el recepcionista te invita a disfrutar del buffet. Tu día termina bien.")
+                            }
+                        }else{
+                            alert("No te alcanza para entrar al Bingo. Pasas todo el día aburrido viendo a los demás huespedes jugar.");
+                        }
+                        j=false;
+                    }else if(choice==3){
+                        alert("Bienvenido al casino, el lugar preferido por los ludopatas.");
+                        let k=true;
+                        while(k){
+                            showBudget(budget);
+                            choice=confirm("¿Deseas apostar todo tu dinero en la ruleta?");
+                            if(choice){
+                                let m=true;
+                                let rouletteResult=null;
+                                while(m){
+                                    choice=Number(prompt("¿Negro o Rojo?\n1. Negro.\n2. Rojo."));
+                                    rouletteResult=Math.floor(Math.random()*2);
+                                    if((choice>0)&&(choice<3)){
+                                        if(choice==1){
+                                            if(rouletteResult==0){
+                                                alert("Escoges Negro.");
+                                                alert("La ruleta cae en Negro.");
+                                                alert("Ganaste!");
+                                                budget*=2;
+                                                showBudget(budget);
+                                            }else{
+                                                alert("Escoges Negro.");
+                                                alert("La ruleta cae en Rojo.");
+                                                alert("Perdiste...");
+                                                budget=0;
+                                                showBudget(budget)
+                                            }
+                                        }else if(choice==2){
+                                            if(rouletteResult==1){
+                                                alert("Escoges Rojo.");
+                                                alert("La ruleta cae en Rojo.");
+                                                alert("Ganaste!");
+                                                budget*=2;
+                                                showBudget(budget);
+                                            }else{
+                                                alert("Escoges Rojo.");
+                                                alert("La ruleta cae en Negro");
+                                                alert("Perdiste...");
+                                                budget=0;
+                                                showBudget(budget);
+                                            }
+                                        }
+                                        m=false;
+                                    }else{
+                                        alert("Introduce una opción válida.")
+                                    }
+                                }
+                                if(budget==0){
+                                    alert("La ludopatia te llevo a perder todo tu dinero, decides volver a tu habitación a llorar en silencio y así termina tu día.");
+                                    k=false;
+                                }
+                            }else{   
+                                alert("Terminas el día en el casino disfrutando del buffet ofrecido.");
+                                k=false;
+                            }
+                            
+                        }
+                        j=false;
+                    }
+                }else{
+                    alert("Introduce una opción válida.")
+                }
+            }
         }else if(colorSelect=="Verde"){
-            alert(colorSelect);
+            let j=true;
+            alert("La actividad para el día de hoy es disfrutar una caminata dirigida por un guía del hotel, esoge tu ropa más cómoda ya que avisan que es una caminata larga.");
+            while(j){
+                choice=Number(prompt("¿Deseas asistir a la caminata guiada, o prefieres salir a conocer el pueblo de Macondo por tu cuenta?\n1. Asistir a la caminata con el guía del hotel.\n2. Conocer Macondo por tu cuenta."));
+                if((choice>0)&&(choice<3)){
+                    if(choice==1){
+                        alert("La caminata inicia de manera aburrida, te comienzas a sentir cansado rapidamente.");
+                        alert("El guía del hotel los adentra en un bosque lo cuál empieza a ser sospechoso...");
+                        alert("Llegan a la cascada más hermosa que tus ojos han podido ver, aprovechas para fotografiar esta maravilla natural.");
+                        alert("Finalmente vuelven sanos al hotel. Terminas el día cansado pero maravillado porl o que viste.").
+                        j=false;
+                    }else if(choice==2){
+                        alert("Decides no ir a la caminata, en lugar de eso salir a conocer el pueblo de Macondo.");
+                        alert("Llegas al pueblo y todo es hermoso, las casas tienen una preciosa arquitectura y disfrutas saludar a algunos locales.");
+                        alert("Como no hablas Macondes, rapidamente descubren que eres extranjero.");
+                        alert("Un grupo de bandidos locales descubren que eres extranjero y te abordan en su camioneta. Te suben encañonado con una pistola y te llevan a un lugar lejano.");
+                        alert("Te piden 300000 para dejarte en libertad.");
+                        let steal=300000;
+                        if(steal>budget){
+                            showBudget(budget);
+                            alert("Los bandidos deciden matarte por no tener el dinero suficiente. La delincuencia ha acabado con tu vida.");
+                            death=true;
+                            return;
+                        }else{
+                            alert("Decides pagarles para que te liberen, no queda más opción cuando tienes una pistola ne la cabeza.");
+                            budget-=300000;
+                            showBudget(budget);
+                            alert("Vuelves al hotel asustado, termina tu día con un poco de pánico.");
+                        }
+                        j=false;
+                    }
+                }else{
+                    alert("Introduce una opción válida.");
+                }
+            }
         }
     }
 }
@@ -201,7 +355,11 @@ const macondoDays=function(){
 const showFinalMessage=function(days,budget,death){
     let life= death==false ? "Regresaste vivo de tus vacaciones" : "Moriste en tus vacaciones";
     let expenses=2500000-budget;
-    alert("Llegaste al final de tu aventura.\nEstuviste en Macondo un total de "+days+" días.\nGastaste un total de "+expenses+" pesos.\n"+life)
+    if(expenses>=0){
+        alert("Llegaste al final de tu aventura.\nEstuviste en Macondo un total de "+days+" días.\nGastaste un total de "+expenses+" pesos.\n"+life);
+    }else{
+        alert("Llegaste al final de tu aventura.\nEstuviste en Macondo un total de "+days+" días.\nGanaste un total de "+(expenses*-1)+" pesos.\n"+life);
+    }
 }
 
 const showBudget=function(budget){
